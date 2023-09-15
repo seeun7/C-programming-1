64149663fc7b94f5a515456a0c1345d54df914e4
seeun7 <126846674+seeun7@users.noreply.github.com>
2023-09-16 01:42:36 +0900

    Add File

---
+++ hw1.c
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main(void)
+{
+    int num1, num2;
+    printf("Input two integers: ");
+    scanf("%d %d", &num1, &num2);
+    int num3 = num1 & num2;
+    int num4 = num1 | num2;
+    int num5 = num1 ^ num2;
+    printf("%d & %d = %d\n", num1, num2, num3);
+    printf("%d | %d = %d\n", num1, num2, num4);
+    printf("%d ^ %d = %d\n", num1, num2, num5);
+    return 0;
+}
